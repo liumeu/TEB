d41e8d6fd679c52e75b35bb2131e745464d7f670
liumeu <meushlubo@gmail.com>
2024-10-06 10:10:45 +0200

    to 1 file

--- 061024.py
+++ 061024.py
@@ -1,15 +1,29 @@
-print('Kalkulator BMI')
+import datetime
+# print('Kalkulator BMI')
 
-masa = int(input('Podaj swoją wagę: '))
-wzrost = int(input('Podaj swój wzrost: '))
+# masa = int(input('Podaj swoją wagę: '))
+# wzrost = int(input('Podaj swój wzrost: '))
 
-bmi = masa/wzrost**2
+# bmi = masa/wzrost**2
 
-print('Twoje BMI wynosi: ', bmi)
+# print('Twoje BMI wynosi: ', bmi)
 
-if bmi > 18 and bmi < 25:
-    print('Jestes w normie')
-elif bmi < 18:
-    print('Niedowaga')
-elif bmi > 25:
-    print('Nadwaga')
+# if bmi > 18 and bmi < 25:
+#     print('Jestes w normie')
+# elif bmi < 18:
+#     print('Niedowaga')
+# elif bmi > 25:
+#     print('Nadwaga')
+
+# import datetime
+# tekst = 'Litwo, Ojczyzno moja! ty jesteś jak zdrowie;Ile cię trzeba cenić, ten tylko się dowie,Kto cię stracił. Dziś piękność twą w całej ozdobieWidzę i opisuję, bo tęsknię po tobie.'
+
+# male_litery = tekst.lower()
+
+# samogloski = 'aeiouyąęó'
+
+# ilosc = sum(1 for liter in male_litery if liter in samogloski)
+# print(ilosc)
+
+
+print(datetime.datetime.now())
--- 061024_2.py
+++
@@ -1,8 +0,0 @@
-tekst = 'Litwo, Ojczyzno moja! ty jesteś jak zdrowie;Ile cię trzeba cenić, ten tylko się dowie,Kto cię stracił. Dziś piękność twą w całej ozdobieWidzę i opisuję, bo tęsknię po tobie.'
-
-male_litery = tekst.lower()
-
-samogloski = 'aeiouyąęó'
-
-ilosc = sum(1 for liter in male_litery if liter in samogloski)
-print(ilosc)
