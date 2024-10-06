5a95de6c0289a1a1e570382060e6972745be98ae
liumeu <meushlubo@gmail.com>
2024-10-06 09:58:37 +0200

    bmi

--- 061024.py
+++ 061024.py
@@ -1,1 +1,15 @@
-test222
+print('Kalkulator BMI')
+
+masa = int(input('Podaj swoją wagę: '))
+wzrost = int(input('Podaj swój wzrost: '))
+
+bmi = masa/wzrost**2
+
+print('Twoje BMI wynosi: ', bmi)
+
+if bmi > 18 and bmi < 25:
+    print('Jestes w normie')
+elif bmi < 18:
+    print('Niedowaga')
+elif bmi > 25:
+    print('Nadwaga')
